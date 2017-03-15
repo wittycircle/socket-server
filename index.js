942379d22b7cc72146213bc330303e3056c4d7f1
Raphael Dantzer <rdantzer@student.42.fr>
2017-03-15 06:29:01 +0100

    :construction: authentication and basic routing done

---
+++ index.js
@@ -0,0 +1,29 @@
+/**
+ * Created by rdantzer on 15/03/17.
+ */
+
+'use strict';
+
+
+const io = require('socket.io')();
+
+io.on('connection', socket => {
+    console.log(socket.id);
+
+    socket.isAuthenticated = false;
+
+    const middlewares = require('./lib/middlewares')(socket),
+        streams = require('./lib/streams')(socket);
+
+    socket.use(middlewares.log);
+    socket.use(middlewares.authenticate);
+    socket.use(middlewares.validate);
+
+    socket.on('client::authenticate', streams.authenticate);
+    socket.on('client::request::latest_messages', console.log);
+    socket.on('client::request::invite', console.log);
+    socket.on('client::autocomplete', streams.autocomplete);
+
+});
+
+io.listen(process.env.PORT || 3200);
--- server.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by rdantzer on 15/03/17.
- */
-
-'use strict';
-
-const io = require('socket.io')
