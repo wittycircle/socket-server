ab636ceff329ea7d6dff7a9799615f03dee2a002
Raphael Dantzer <rdantzer@student.42.fr>
2017-04-01 15:48:27 +0200

    :construction: more and more rxjs rewrite, message ok but no persistence of data

--- index.js
+++ index.js
@@ -5,30 +5,39 @@
 'use strict';
 
 const io = require('socket.io')({
-        transports: ['websocket']
+        // transports: ['websocket']
     }),
-    cache = require('./lib/cache').init(),
-    events = require('./lib/events'),
-    dispatch = require('./lib/dispatch')(events.data);
+    cache = require('./lib/cache').init();
 
-io.on('connection', socket => {
-    socket.isAuthenticated = false;
+const opts = require('./lib/config');
 
-    const middlewares = require('./lib/middlewares')(socket),
-        streams = require('./lib/streams')(socket);
+const Dispatcher = require('./lib/core/dispatch'),
+    dispatcher = new Dispatcher(io, opts);
 
-    socket.use(middlewares.log);
-    socket.use(middlewares.authenticate);
-    socket.use(middlewares.validate);
+dispatcher.start();
 
-    socket.on('client::authenticate', streams.authenticate);
-    socket.on('client::autocomplete', streams.autocomplete);
 
-    socket.on('client::request::latest_messages', console.log);
-    socket.on('client::request::invite', console.log);
-    socket.on('client::messages', streams.message);
-
-    dispatch.register(socket)
-});
+// io.on('connection', socket => {
+//     socket.isAuthenticated = false;
+//
+//     const middlewares = require('./lib/middlewares')(socket),
+//         streams = require('./lib/streams')(socket);
+//
+//     socket.use(middlewares.log);
+//     socket.use(middlewares.authenticate);
+//     socket.use(middlewares.validate);
+//
+//     socket.on('client::authenticate', streams.authenticate);
+//     socket.on('client::autocomplete', streams.autocomplete);
+//
+//     socket.on('client::request::latest_messages', console.log);
+//     socket.on('client::request::invite', console.log);
+//     socket.on('client::request::user', streams.user_info);
+//     socket.on('client::messages', streams.message);
+//
+//     dispatch.register(socket)
+//
+//     require('./lib/core');
+// });
 
 io.listen(process.env.PORT || 3200);
