f9b831654c93ed9d8fd6798f821df6bf6271a81c
Raphael Dantzer <rdantzer@student.42.fr>
2017-04-03 06:44:43 +0200

    :books: Code is now 100% documented

--- index.js
+++ index.js
@@ -9,35 +9,21 @@ const io = require('socket.io')({
     }),
     cache = require('./lib/cache').init();
 
+//Import options
 const opts = require('./lib/config');
 
 const Dispatcher = require('./lib/core/dispatch'),
+    /**
+     * Create a new dispatcher which handles redis and socket.io as streams with observable magic
+     */
     dispatcher = new Dispatcher(io, opts);
 
+/**
+ * Take a ride into madness
+ */
 dispatcher.start();
 
-
-// io.on('connection', socket => {
-//     socket.isAuthenticated = false;
-//
-//     const middlewares = require('./lib/middlewares')(socket),
-//         streams = require('./lib/streams')(socket);
-//
-//     socket.use(middlewares.log);
-//     socket.use(middlewares.authenticate);
-//     socket.use(middlewares.validate);
-//
-//     socket.on('client::authenticate', streams.authenticate);
-//     socket.on('client::autocomplete', streams.autocomplete);
-//
-//     socket.on('client::request::latest_messages', console.log);
-//     socket.on('client::request::invite', console.log);
-//     socket.on('client::request::user', streams.user_info);
-//     socket.on('client::messages', streams.message);
-//
-//     dispatch.register(socket)
-//
-//     require('./lib/core');
-// });
-
+/**
+ * Starts socket.io
+ */
 io.listen(process.env.PORT || 3200);
